74866e9eff8f7e82691f54c07712d9747ebfd96e
Technopig <127978232+Technopig1992@users.noreply.github.com>
2026-02-13 18:58:22 +0000

    moved Stubs to TestStubs, FullMatchFlowWithPreferencesTests

--- LegaciesBot.Tests/FullMatchFlowTests.cs
+++ LegaciesBot.Tests/FullMatchFlowTests.cs
@@ -1,7 +1,11 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using Xunit;
 using LegaciesBot.Core;
 using LegaciesBot.GameData;
 using LegaciesBot.Services;
-
 
 public class FullMatchFlowTests
 {
@@ -86,63 +90,5 @@ public class FullMatchFlowTests
         Assert.True(game.Finished);
         Assert.NotEmpty(changes);
         Assert.NotEmpty(history.History);
-    }
-}
-
-public class DummyGatewayClient : IGatewayClient
-{
-    public Task<ITextChannel?> GetTextChannelAsync(ulong id) =>
-        Task.FromResult<ITextChannel?>(null);
-}
-
-public class EloStub : IEloService
-{
-    public Dictionary<ulong, int> ApplyTeamResult(
-        List<Player> teamA,
-        List<Player> teamB,
-        bool teamAWon,
-        PlayerStatsService stats)
-    {
-        return teamA.Concat(teamB)
-            .ToDictionary(p => p.DiscordId, p => 5);
-    }
-}
-
-public class FactionRegistryStub : IFactionRegistry
-{
-    public IEnumerable<Faction> All => FactionRegistry.All;
-}
-
-public class DefaultPreferencesStub : IDefaultPreferences
-{
-    public List<string> Factions => FactionRegistry.All.Select(f => f.Name).ToList();
-}
-
-public class FactionAssignmentStub : IFactionAssignmentService
-{
-    public void AssignFactionsToTeam(Team team, HashSet<TeamGroup> allowedGroups)
-    {
-        var factions = FactionRegistry.All
-            .Where(f => allowedGroups.Contains(f.Group))
-            .Take(team.Players.Count)
-            .ToList();
-
-        team.AssignedFactions.Clear();
-        team.AssignedFactions.AddRange(factions);
-    }
-}
-
-public class MatchHistoryAdapter : IMatchHistoryService
-{
-    private readonly MatchHistoryService _inner;
-
-    public MatchHistoryAdapter(MatchHistoryService inner)
-    {
-        _inner = inner;
-    }
-
-    public void RecordMatch(Game game, int scoreA, int scoreB, Dictionary<ulong, int> eloChanges)
-    {
-        _inner.RecordMatch(game, scoreA, scoreB, eloChanges);
     }
 }
--- LegaciesBot.Tests/FullMatchFlowWithPreferencesTests.cs
+++ LegaciesBot.Tests/FullMatchFlowWithPreferencesTests.cs
@@ -1,6 +1,169 @@
-namespace LegaciesBot.Tests;
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using Xunit;
+using LegaciesBot.Core;
+using LegaciesBot.GameData;
+using LegaciesBot.Services;
 
 public class FullMatchFlowWithPreferencesTests
 {
-    
+    private PlayerRegistryService FreshRegistry()
+    {
+        if (File.Exists("players.json"))
+            File.Delete("players.json");
+
+        return new PlayerRegistryService();
+    }
+
+    private MatchHistoryService FreshHistory()
+    {
+        if (File.Exists("match_history.json"))
+            File.Delete("match_history.json");
+
+        return new MatchHistoryService();
+    }
+
+    private readonly List<(ulong id, string name)> Players = new()
+    {
+        (1, "Boggywoggy"),
+        (2, "Konan"),
+        (3, "Dia"),
+        (4, "Helsac"),
+        (5, "Nick"),
+        (6, "Grom"),
+        (7, "Linaz"),
+        (8, "TheG"),
+        (9, "Technopig"),
+        (10, "Enclop"),
+        (11, "Lukas"),
+        (12, "Alan"),
+        (13, "Royce"),
+        (14, "Petertros"),
+        (15, "Dragozer"),
+        (16, "Madsen")
+    };
+
+    private readonly Dictionary<ulong, List<string>> Prefs;
+
+    public FullMatchFlowWithPreferencesTests()
+    {
+        var all = FactionRegistry.All.Select(f => f.Name).ToList();
+
+        Prefs = new()
+        {
+            [1] = new() { "Fel Horde", "An'qiraj", "Stormwind", "Lordaeron", "Druids", "Scourge" },
+
+            [2] = new() { "Warsong", "An'qiraj", "Illidari", "Sentinels", "Scourge", "Fel Horde", "Kul'tiras" },
+
+            [3] = all
+                .Where(f => f != "Scourge" && f != "Gilneas" && f != "Sunfury")
+                .ToList(),
+
+            [4] = new() { "Lordaeron", "Skywall", "Stormwind" },
+
+            // Nick corrected: ONLY these three
+            [5] = new() { "Dalaran", "Legion", "Druids" },
+
+            [6] = new()
+            {
+                "Ironforge", "Stormwind", "The Exodar", "Druids", "Lordaeron",
+                "Kul'tiras", "Illidari", "Gilneas", "Sentinels", "Black Empire", "Legion"
+            },
+
+            [7] = new()
+            {
+                "Skywall", "Scourge", "An'qiraj", "Sentinels", "The Exodar",
+                "Quel'thalas", "Illidari", "Fel Horde", "Dalaran", "Ironforge", "Kul'tiras"
+            },
+
+            [8] = new() { "Lordaeron", "Sentinels", "Ironforge" },
+
+            [9] = new() { "Dalaran", "Quel'thalas", "Kul'tiras", "Illidari", "Stormwind" },
+
+            [10] = new() { "Warsong", "Skywall", "Dalaran", "Scourge", "Kul'tiras" },
+
+            [11] = new() { "Gilneas", "Lordaeron", "Quel'thalas", "Frostwolf", "Fel Horde", "Kul'tiras" },
+
+            [12] = new() { "Illidari", "Legion", "Druids", "Quel'thalas", "Lordaeron" },
+
+            [13] = new() { "Fel Horde", "Scourge", "Frostwolf", "Kul'tiras", "Lordaeron" },
+
+            [14] = new() { "Kul'tiras", "Lordaeron", "Stormwind", "The Exodar", "Quel'thalas" },
+
+            [15] = new()
+            {
+                "Dalaran", "Scourge", "Fel Horde", "Warsong",
+                "Sentinels", "Stormwind", "Sunfury"
+            },
+
+            [16] = new()
+            {
+                "Lordaeron", "Stormwind", "Warsong", "Sunfury",
+                "Gilneas", "Skywall", "The Exodar"
+            }
+        };
+    }
+
+    [Fact]
+    public void FullMatchFlow_WithPreferences_WorksEndToEnd()
+    {
+        var registry = FreshRegistry();
+        var lobbyService = new LobbyService();
+        var history = FreshHistory();
+
+        var elo = new EloStub();
+        var factionRegistry = new FactionRegistryStub();
+        var defaultPrefs = new DefaultPreferencesStub();
+        var factionAssignment = new FactionAssignmentStub();
+
+        var gameService = new GameService(
+            new DummyGatewayClient(),
+            new MatchHistoryAdapter(history),
+            elo,
+            factionAssignment,
+            factionRegistry,
+            defaultPrefs
+        );
+
+        foreach (var (id, name) in Players)
+            registry.RegisterPlayer(id, name);
+
+        foreach (var (id, name) in Players)
+            lobbyService.JoinLobby(id, name);
+
+        foreach (var (id, _) in Players)
+            lobbyService.UpdatePreferences(id, Prefs[id]);
+
+        var lobby = lobbyService.CurrentLobby;
+
+        var (teamA, teamB) = DraftService.CreateBalancedTeams(lobby.Players);
+
+        var game = gameService.StartGame(lobby, teamA, teamB);
+
+        var stats = new PlayerStatsService();
+
+        var changes = gameService.SubmitScore(game, 4, 2, stats);
+
+        foreach (var player in teamA.Players)
+        {
+            var faction = teamA.AssignedFactions
+                .FirstOrDefault(f => Prefs[player.DiscordId].Contains(f.Name));
+
+            Assert.NotNull(faction);
+        }
+
+        foreach (var player in teamB.Players)
+        {
+            var faction = teamB.AssignedFactions
+                .FirstOrDefault(f => Prefs[player.DiscordId].Contains(f.Name));
+
+            Assert.NotNull(faction);
+        }
+
+        Assert.True(game.Finished);
+        Assert.NotEmpty(changes);
+        Assert.NotEmpty(history.History);
+    }
 }
