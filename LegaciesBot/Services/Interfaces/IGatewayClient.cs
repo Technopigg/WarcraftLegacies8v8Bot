1cb89430239ef6f73e688ac7ef72973bf41e9eb3
Technopig <127978232+Technopig1992@users.noreply.github.com>
2026-03-31 19:37:49 +0100

    GatewayClient additions

--- LegaciesBot/Services/Interfaces/IGatewayClient.cs
+++ LegaciesBot/Services/Interfaces/IGatewayClient.cs
@@ -1,6 +1,19 @@
+using NetCord;
+using NetCord.Rest;
+
 namespace LegaciesBot.Services;
 
 public interface IGatewayClient
 {
     Task<ITextChannel?> GetTextChannelAsync(ulong channelId);
+
+    Task<RestGuild> GetGuildAsync(ulong guildId, bool withCounts = false);
+
+    Task<Role> CreateRoleAsync(ulong guildId, string name);
+
+    Task AddRoleToMemberAsync(ulong guildId, ulong userId, ulong roleId);
+
+    Task RemoveRoleFromMemberAsync(ulong guildId, ulong userId, ulong roleId);
+
+    Task DeleteRoleAsync(ulong guildId, ulong roleId);
 }
--- LegaciesBot/Services/Interfaces/RealGatewayClient.cs
+++ LegaciesBot/Services/Interfaces/RealGatewayClient.cs
@@ -1,5 +1,6 @@
+using NetCord;
 using NetCord.Gateway;
-
+using NetCord.Rest;
 
 namespace LegaciesBot.Services;
 
@@ -20,5 +21,33 @@ public class RealGatewayClient : IGatewayClient
             return new RealTextChannel(text);
 
         return null;
+    }
+
+    public Task<RestGuild> GetGuildAsync(ulong guildId, bool withCounts = false)
+    {
+        return _client.Rest.GetGuildAsync(guildId, withCounts);
+    }
+
+    public Task<Role> CreateRoleAsync(ulong guildId, string name)
+    {
+        return _client.Rest.CreateGuildRoleAsync(guildId, new RoleProperties
+        {
+            Name = name
+        });
+    }
+
+    public Task AddRoleToMemberAsync(ulong guildId, ulong userId, ulong roleId)
+    {
+        return _client.Rest.AddGuildUserRoleAsync(guildId, userId, roleId);
+    }
+
+    public Task RemoveRoleFromMemberAsync(ulong guildId, ulong userId, ulong roleId)
+    {
+        return _client.Rest.RemoveGuildUserRoleAsync(guildId, userId, roleId);
+    }
+
+    public Task DeleteRoleAsync(ulong guildId, ulong roleId)
+    {
+        return _client.Rest.DeleteGuildRoleAsync(guildId, roleId);
     }
 }
